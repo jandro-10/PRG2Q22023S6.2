f6b17283885ad48f4f1d527ad5ac7c710cb36af7
frlopez88 <68876977+frlopez88@users.noreply.github.com>
2023-05-29 20:43:09 -0600

    Serializacion de Circulos
    Se crean listas para guardar en el telefono y se puedan leer en un control de List View

--- MiPrimerApp/MiPrimerApp/MiPrimerApp/Models/Circulo.cs
+++ MiPrimerApp/MiPrimerApp/MiPrimerApp/Models/Circulo.cs
@@ -4,18 +4,21 @@ using System.Text;
 
 namespace MiPrimerApp.Models
 {
+    [Serializable]
     public class Circulo
     {
 
         public double radio { get; set; }
         public double perimetro { get; set; }
         public double area { get; set; }
+        public string getToString { get; set; }
 
         public Circulo(double r) {
             
             radio = r;
             calcularArea();
             calcularPerimetro();
+            getToString = $"Area: {area} -  Perimetro: {perimetro} ";
         } 
 
         public void calcularArea()
@@ -30,7 +33,7 @@ namespace MiPrimerApp.Models
 
         public override string ToString()
         {
-            return $" Area: {area} -  Perimetro: {perimetro} ";
+            return getToString;
         }
 
     }
--- MiPrimerApp/MiPrimerApp/MiPrimerApp/ViewModels/viewModelCirculo.cs
+++ MiPrimerApp/MiPrimerApp/MiPrimerApp/ViewModels/viewModelCirculo.cs
@@ -1,7 +1,10 @@
 ﻿using MiPrimerApp.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using Xamarin.Forms;
 
@@ -12,16 +15,46 @@ namespace MiPrimerApp.ViewModels
 
         public viewModelCirculo() {
 
+
+            //Apertura de Archivo Lista de Ciculos
+
+            try
+            {
+                /*Abir y leer el archivo */
+                byte[] data = File.ReadAllBytes(ruta);
+                MemoryStream memory = new MemoryStream(data);
+                BinaryFormatter formater = new BinaryFormatter();
+                lista = (ObservableCollection<Circulo>)formater.Deserialize(memory);
+                memory.Close();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Sin archivo por cargar de Personas");
+
+            }
+
             crear = new Command(() => {
 
                 Circulo c = new Circulo(radio);
                 Resultado = c.ToString();
-            
+                lista.Add(c);
+
+                //Salvar la Lista en mi telefono
+                BinaryFormatter formateador = new BinaryFormatter();
+                MemoryStream memoria = new MemoryStream();
+                formateador.Serialize(memoria, lista);
+                byte[] datoSerializados = memoria.ToArray();
+                memoria.Close();
+                
+                File.WriteAllBytes(ruta, datoSerializados);
+
             });
 
 
         }
 
+        string ruta = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "circulos.bin");
 
         double radio;
 
@@ -50,9 +83,9 @@ namespace MiPrimerApp.ViewModels
         
         }
 
+        public ObservableCollection<Circulo> lista { get; set; } = new ObservableCollection<Circulo>();
 
         public Command crear { get; }
-
         
         public event PropertyChangedEventHandler PropertyChanged;
     }
